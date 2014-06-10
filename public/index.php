28e5e6ce7c219d83b2338fca31e65f62058b6bad
ethanf22 <efederman22@gmail.com>
2014-06-10 08:02:05 -0700

    Init

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
 
+error_reporting(E_ALL);
+ini_set('display_errors', True);
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
